931689044b700c5a7311ea58910b7cd1fa9ae92d
NikkiCooper <nikki.lynn.cooper@gmail.com>
2025-09-12 17:28:15 -0500

    Removed  trailing whitespace

--- debug_utils.py
+++ debug_utils.py
@@ -3,6 +3,5 @@ import inspect
 
 def debug(*args):
     frame = inspect.currentframe().f_back
-    info = inspect.getframeinfo(frame)  
+    info = inspect.getframeinfo(frame)
     print(f"{info.filename}, line {info.lineno}, in {info.function}: ", *args)
-    
