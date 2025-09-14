a46fd38c26354b9355e258908483450f5eeb66f6
NikkiCooper <nikki.lynn.cooper@gmail.com>
2025-09-13 21:02:57 -0500

    Convert tabs to spaces

--- pyv2.py
+++ pyv2.py
@@ -15,78 +15,77 @@ from EventHandler import EventHandler
 from FindVideos import FindVideos
 
 def listActiveMonitors():
-	"""
-	Executes a system command to list active monitors and prints the result.
+    """
+    Executes a system command to list active monitors and prints the result.
 
-	This function uses the `subprocess.run` method to execute the "xrandr --listactivemonitors"
-	command and captures its output. The output is then printed to the standard output. This
-	function is useful for retrieving information about currently active monitors on a system.
+    This function uses the `subprocess.run` method to execute the "xrandr --listactivemonitors"
+    command and captures its output. The output is then printed to the standard output. This
+    function is useful for retrieving information about currently active monitors on a system.
 
-	Returns:
-	    None
-	"""
-	result = subprocess.run(["xrandr", "--listactivemonitors"], capture_output=True, text=True)
-	print(result.stdout)
+    Returns:
+        None
+    """
+    result = subprocess.run(["xrandr", "--listactivemonitors"], capture_output=True, text=True)
+    print(result.stdout)
 
 def main():
-	"""
-	Main module for a Linux-based video player written in Python. The script initializes
-	console color settings, handles command-line options, discovers video files, and plays
-	media using specified configurations.
+    """
+    Main module for a Linux-based video player written in Python. The script initializes
+    console color settings, handles command-line options, discovers video files, and plays
+    media using specified configurations.
 
-	Functions:
-	    main: Entry point for the video player program.
+    Functions:
+        main: Entry point for the video player program.
 
-	Parameters:
-	    None
+    Parameters:
+        None
 
-	Raises:
-	    None
+    Raises:
+        None
 
-	Returns:
-	    None
-	"""
-	# Create a Bcolors instance to give us colors in the console.
-	bcolors = Bcolors()
-	#bcolors.clear()
-	Version = '0.50'
-	program_path = __file__
-	program_name = os.path.basename(program_path)
-	# Retrieve all command line arguments and default variables, as well as user specified directories
-	print(
-		f"{bcolors.RESET}\n{bcolors.Green_f}{program_name} "
-		f"version {bcolors.Cyan_f}{Version}{bcolors.RESET}"
-		f" A video player for Linux written in Python.{bcolors.RESET}")
+    Returns:
+        None
+    """
+    # Create a Bcolors instance to give us colors in the console.
+    bcolors = Bcolors()
+    #bcolors.clear()
+    Version = '0.60'
+    program_path = __file__
+    program_name = os.path.basename(program_path)
+    # Retrieve all command line arguments and default variables, as well as user specified directories
+    print(
+        f"{bcolors.RESET}\n{bcolors.Green_f}{program_name} "
+        f"version {bcolors.Cyan_f}{Version}{bcolors.RESET}"
+        f" A video player for Linux written in Python.{bcolors.RESET}")
 
-	#opts, pathList, reader = cliOptions(bcolors)
-	opts = cmdLineOptions()
+    #opts, pathList, reader = cliOptions(bcolors)
+    opts = cmdLineOptions()
 
-	if opts.listActiveMonitors:
-		listActiveMonitors()
-		exit()
+    if opts.listActiveMonitors:
+        listActiveMonitors()
+        exit()
 
-	# Create a FindVideos instance, populate Videos.videoList with the path/filename of all found playable media.
-	Videos = FindVideos(opts)
+    # Create a FindVideos instance, populate Videos.videoList with the path/filename of all found playable media.
+    Videos = FindVideos(opts)
 
-	if opts.printIgnoreList or opts.printVideoList:
-		if opts.printIgnoreList:
-			Videos.print_ignores()
-		if opts.printVideoList:
-			Videos.videoList_print()
-		exit()
+    if opts.printIgnoreList or opts.printVideoList:
+        if opts.printIgnoreList:
+            Videos.print_ignores()
+        if opts.printVideoList:
+            Videos.videoList_print()
+        exit()
 
-	# Create a PlayVideo instance, pass our cli arguments and variables to it, also pass our videoList created
-	# by FindVideos().  Finally, pass a bcolors object to it so we can have some colors in the console.
-	playVid = PlayVideo(opts, Videos.videoList, bcolors)
-	# Clear the console screen
-	#bcolors.clear()
-	if opts.shuffle:
-		playVid.shuffleVideoList()
-	# Create an instance of a pygame EventHandler and run it.
-	eventHandler = EventHandler(playVid)
-	# Finally, play all the videos that are specified in playVid.videoList
-	playVid.play(eventHandler)
+    # Create a PlayVideo instance, pass our cli arguments and variables to it, also pass our videoList created
+    # by FindVideos().  Finally, pass a bcolors object to it so we can have some colors in the console.
+    playVid = PlayVideo(opts, Videos.videoList, bcolors)
+    # Clear the console screen
+    #bcolors.clear()
+    if opts.shuffle:
+        playVid.shuffleVideoList()
+    # Create an instance of a pygame EventHandler and run it.
+    eventHandler = EventHandler(playVid)
+    # Finally, play all the videos that are specified in playVid.videoList
+    playVid.play(eventHandler)
 
 if __name__ == "__main__":
-	main()
-	
+    main()
