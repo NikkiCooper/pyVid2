6cfd2d16d6fa02d4a2da9bd8cb9a17b24cf1f147
Nikki Cooper <nikki.lynn.cooper@gmail.com>
2026-04-10 18:38:33 -0500

    Removed unnecessary shebang

--- IRRemoteControl.py
+++ IRRemoteControl.py
@@ -1,5 +1,4 @@
-#!/usr/bin/env python
-#   IRRemoteControl.py Copyright (c) 2025 Nikki Cooper
+#   IRRemoteControl.py Copyright (c) 2026 Nikki Cooper
 #
 #  This program and the accompanying materials are made available under the
 #  terms of the GNU Lesser General Public License, version 3.0 which is available at
